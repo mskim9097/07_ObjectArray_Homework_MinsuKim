41ada486da8d4ad74c6e58c897ae9458fa650cda
MINSU KIM <mskim9097@gmail.com>
2022-12-21 15:12:30 +0900

    20221221 commit

---
+++ src/com/hw4/controller/MemberController.java
@@ -0,0 +1,15 @@
+package com.hw4.controller;
+
+import com.hw4.model.vo.Member;
+
+public class MemberController {
+	
+	public static final int SIZE = 10;
+	
+	private int memberCount = 0;
+	
+	Member[] mem = new Member[10];
+	
+	public int getMemberCount() {}
+
+}
---
+++ src/com/hw4/run/Run.java
@@ -0,0 +1,11 @@
+package com.hw4.run;
+
+public class Run {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
