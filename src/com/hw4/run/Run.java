8ae4c9457368a7bebbe3568ad8b6ca71f032af1e
MINSU KIM <mskim9097@gmail.com>
2023-01-01 21:50:40 +0900

    20230101 home commit

--- src/com/hw4/run/Run.java
+++ src/com/hw4/run/Run.java
@@ -1,10 +1,14 @@
 package com.hw4.run;
 
+import com.hw4.view.MemberMenu;
+
 public class Run {
 	
 	public static void main(String[] args) {
 		
+		MemberMenu m = new MemberMenu();
 		
+		m.mainMenu();
 		
 	}
 
